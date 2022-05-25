a2852b78acd711d6d3f7bfc029386e681bca99b1
Andrey_Soin <soinproduction@gmail.com>
2022-05-25 07:39:41 +0300

    full project

--- source/js/common.js
+++ source/js/common.js
@@ -1,7 +1,68 @@
+
+const burgerBtns = [...document.querySelectorAll(".burger-btn")];
+const replaceElement = [...document.querySelectorAll("[data-replace]")];
+const replaceSelect = [...document.querySelectorAll("[data-replace-select]")];
+const leftInner = document.querySelector("[left-inner]");
+const desktopInner = document.querySelector("[data-desktop-inner]");
+const mobileInner = document.querySelector("[data-mobile-inner]");
+const breakpoint = 1024;
+
+const body = document.body
+
+
+const changeLocation = (elements, parentMobile, parentDesktop) => {
+  const containerWidth = document.documentElement.clientWidth;
+  for (const element of elements) {
+    if (containerWidth < breakpoint) {
+      parentMobile.insertAdjacentElement("beforeend", element);
+    };
+    if (containerWidth > breakpoint) {
+      parentDesktop.insertAdjacentElement("beforeend", element);
+    }
+  }
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  changeLocation(replaceElement, mobileInner, desktopInner);
+  changeLocation(replaceSelect, leftInner, desktopInner);
+  setHeaderHeight();
+});
+
+window.addEventListener("resize", () => {
+  changeLocation(replaceElement, mobileInner, desktopInner);
+  changeLocation(replaceSelect, leftInner, desktopInner);
+  setHeaderHeight();
+
+});
+
+
+
+
+const toggleClassOnClick = function(element,button) {
+  element.classList.toggle('active');
+  button.classList.toggle('active');
+};
+
+
+for (const burgerBtn of burgerBtns) {
+  burgerBtn.addEventListener('click', function(){
+    toggleClassOnClick(mobileInner,burgerBtn)
+  });
+}
+
+
+
+function setHeaderHeight() {
+  let header = document.querySelector('header'),
+      headerHeight = header.clientHeight + 'px';
+  body.style.setProperty('--header-min-height', headerHeight)
+}
+
 let slider = new Swiper(".logo-section__slider", {
   slidesPerView: "auto",
   spaceBetween: 30,
   loop: true,
+  observer: true,
   speed: 5000,
   autoplay: {
     delay: 0,
@@ -28,13 +89,27 @@ let slider = new Swiper(".logo-section__slider", {
       slidesPerView: "auto",
       spaceBetween: 20,
       loop: true,
+      slideToClickedSlide: true,
+      observer: true,
       speed: 8000,
       autoplay: {
         delay: 0,
       },
       pagination: {
-        el: '.about__pagination',
-        type: 'bullets',
+        el: ".about__pagination",
+        type: "bullets",
+        clickable: true,
+      },
+      on: {
+        init() {
+          this.el.addEventListener('mouseenter', () => {
+            this.autoplay.stop();
+          });
+
+          this.el.addEventListener('mouseleave', () => {
+            this.autoplay.start();
+          });
+        }
       },
     });
   };
@@ -62,15 +137,29 @@ let slider = new Swiper(".logo-section__slider", {
     slider = new Swiper(".our-team__container", {
       slidesPerView: "auto",
       spaceBetween: 20,
+      observer: true,
       centeredSlides: true,
       loop: true,
+      slideToClickedSlide: true,
       speed: 1500,
       autoplay: {
         delay: 4000,
       },
       pagination: {
-        el: '.our-team__pagination',
-        type: 'bullets',
+        el: ".our-team__pagination",
+        type: "bullets",
+        clickable: true,
+      },
+      on: {
+        init() {
+          this.el.addEventListener('mouseenter', () => {
+            this.autoplay.stop();
+          });
+
+          this.el.addEventListener('mouseleave', () => {
+            this.autoplay.start();
+          });
+        }
       },
     });
   };
@@ -78,9 +167,6 @@ let slider = new Swiper(".logo-section__slider", {
   breakpoint.addListener(breakpointChecker);
   breakpointChecker();
 })();
-
-
-
 
 const select = document.querySelectorAll(".select");
 if (select.length) {
@@ -94,10 +180,65 @@ if (select.length) {
       }
       item.classList.toggle("active");
     });
-    document.addEventListener('click', function(event) {
+    document.addEventListener("click", function (event) {
       if (!item.contains(event.target)) {
-        item.classList.remove('active')
+        item.classList.remove("active");
       }
     });
   });
+}
+
+if (document.querySelector('.nav__list')) {
+  document.querySelector('.nav__list').addEventListener('click', function(e){
+    if (e.target.classList.contains('nav__link--anhor')) {
+      e.preventDefault();
+      mobileInner.classList.remove('active');
+      const id = e.target.getAttribute('href').replace('#', '');
+      let headerHeight = document.querySelector('header').clientHeight;
+      window.scrollTo({
+        top: document.getElementById(id).offsetTop,
+        behavior:"smooth"
+      })
+    }
+  })
+}
+
+if (document.documentElement.clientWidth < 1200) {
+  AOS.init({
+    once: true,
+    throttleDelay: 0,
+    anchorPlacement: 'top-top',
+    delay: 0,
+    offset: 10,
+  });
+} else {
+  AOS.init({
+    // once: true,
+    // throttleDelay: 10,
+
+  });
+}
+
+
+
+
+
+const ethereumButton = document.querySelector('.connect-btn');
+const showAccount = document.querySelector('.connect-btn span');
+
+ethereumButton.addEventListener('click', () => {
+
+  if (typeof window.ethereum === 'undefined') {
+    alert('установите metamask');
+    return;
+  }
+
+  getAccount();
+});
+
+async function getAccount() {
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  const account = accounts[0];
+  showAccount.innerHTML = account;
+  ethereumButton.classList.add('connected');
 }
